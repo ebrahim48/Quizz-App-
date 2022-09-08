32d0d872c82cf3d77f26d88c7a9c947a02dd58c0
ebrahim <ebrahim@softbyteit.com>
2022-09-09 01:29:28 +0600

    finished quizz app

---
+++ lib/pages/result_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quizz_app/pages/question_page.dart';
+import 'package:quizz_app/pages/temp_db.dart';
+
+class ResultPage extends StatelessWidget {
+  static const String routeName = '/result';
+  int rightAnswers = 0;
+  ResultPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    calculateResult();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RESULT'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Correct Answers'),
+            Text('$rightAnswers', style: TextStyle(fontSize: 30),),
+            TextButton(onPressed: () => Navigator.pushReplacementNamed(context, QuestionPage.routeName),
+               child: const Text('GO BACK'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  calculateResult() {
+    for(var map in questionList) {
+      if(map[givenAnswer] == map[rightAnswer]) {
+        rightAnswers++;
+      }
+    }
+  }
+}
+
